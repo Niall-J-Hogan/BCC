2b4634583945b0c8304ab38153a66894d7c0690c
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-09-01 11:53:54 +0100

    added temp background

--- 7hillssite/src/pages/home/page/index.tsx
+++ 7hillssite/src/pages/home/page/index.tsx
@@ -1,11 +1,36 @@
-import { VStack, Heading, Image } from "@chakra-ui/react";
+import {
+  VStack,
+  Heading,
+  Image,
+  useBreakpointValue,
+  Box,
+} from "@chakra-ui/react";
 export const HomePage = () => {
+  const boxSize = useBreakpointValue({
+    base: "300px", // Mobile devices
+    sm: "400px", // Small screens
+    md: "500px", // Medium screens
+    lg: "700px", // Large screens and up
+  });
+
+  //backgroundColor="hsla(238, 100%, 80%, 1)"
   return (
-    <VStack w="100vw" h="100vh" backgroundColor="hsla(238, 100%, 80%, 1)">
-      <Image src={"/7Logo.png"} alt={"Logo"} boxSize={"700px"} margin="auto" />
-      <Heading as="h2" size="2xl" margin="auto" color={"#4900CD"}>
-        + Coming Soon +
-      </Heading>
-    </VStack>
+    <Box
+      backgroundImage={`tempBackround.jpg`} // Replace with your image path
+      opacity={0.9}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      p={4} // Adjust padding as needed
+      h="100vh"
+      w="100vw"
+    >
+      <VStack margin={"auto"} spacing={10}>
+        <Image src={"/7Logo.png"} alt={"Logo"} boxSize={boxSize} />
+        <Heading as="h2" size="2xl" color={"white"}>
+          + Coming Soon +
+        </Heading>
+      </VStack>
+    </Box>
   );
 };
