91eba0c2cf9325bc5fea3351edc0a915ca239cac
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-09-02 11:20:33 +0100

    fix: video to gif

--- src/pages/home/page/index.tsx
+++ src/pages/home/page/index.tsx
@@ -24,76 +24,130 @@ export const HomePage = () => {
   //backgroundColor="hsla(238, 100%, 80%, 1)"
   return (
     <Box
-      position="relative"
-      width="100%"
-      height="100vh"
-      overflow="hidden"
-      zIndex={1}
+      backgroundImage={`Video.gif`} // Replace with your image path
+      opacity={0.9}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      p={4} // Adjust padding as needed
+      h="100vh"
+      w="100vw"
     >
-      <video
-        src="/Video.MOV"
-        muted
-        autoPlay
-        loop
-        style={{
-          height: "100%", // Fill the entire screen height
-          width: "100%", // Fill the entire screen width
-          objectFit: "cover",
-          position: "absolute",
-          top: "0",
-          left: "0",
-          zIndex: -1,
-        }}
-      ></video>
-
       <Box
         position="absolute"
         top="50%"
         left="50%"
         transform="translate(-50%, -50%)"
       >
-        <Image
-          src={"/7Logo.png"}
-          alt={"Logo"}
-          boxSize={boxSize}
-          zIndex={99}
-          marginBottom="20px"
-          opacity={0.9}
-        />
-        <VStack spacing={3} textAlign="center" width="full">
-          <Heading
-            as="h2"
-            size={headingSize}
-            color="#CDAF00"
-            zIndex={99}
-            backgroundColor={"#4900CD"}
-            flexWrap={"nowrap"}
-            dropShadow={"100px"}
-            padding={"5px"}
-          >
-            Sheffield
-          </Heading>
-          <Text
-            fontSize={headingSize}
-            padding={"5px"}
-            color="white"
-            backgroundColor={"#4900CD"}
-          >
-            31/08/2024 - 01/09/2024
-          </Text>
-          <Heading
-            as="h2"
-            size={headingSize}
-            color="#CDAF00"
-            zIndex={99}
-            flexWrap={"nowrap"}
-            padding={"5px"}
-            backgroundColor={"#4900CD"}
-          >
-            Kelham Island Museum
-          </Heading>
+        <VStack margin={"auto"} spacing={10}>
+          <Image src={"/7Logo.png"} alt={"Logo"} boxSize={boxSize} />
+          <VStack spacing={3} textAlign="center" width="full">
+            <Heading
+              as="h2"
+              size={headingSize}
+              color="#CDAF00"
+              zIndex={99}
+              backgroundColor={"#4900CD"}
+              flexWrap={"nowrap"}
+              dropShadow={"100px"}
+              padding={"5px"}
+            >
+              Sheffield
+            </Heading>
+            <Text
+              fontSize={headingSize}
+              padding={"5px"}
+              color="white"
+              backgroundColor={"#4900CD"}
+            >
+              31/08/2024 - 01/09/2024
+            </Text>
+            <Heading
+              as="h2"
+              size={headingSize}
+              color="#CDAF00"
+              zIndex={99}
+              flexWrap={"nowrap"}
+              padding={"5px"}
+              backgroundColor={"#4900CD"}
+            >
+              Kelham Island Museum
+            </Heading>
+          </VStack>
         </VStack>
       </Box>
     </Box>
+    // <Box
+    //   position="relative"
+    //   width="100%"
+    //   height="100vh"
+    //   overflow="hidden"
+    //   zIndex={1}
+    // >
+    //   <video
+    //     src="/Video.MOV"
+    //     muted
+    //     autoPlay
+    //     loop
+    //     style={{
+    //       height: "100%", // Fill the entire screen height
+    //       width: "100%", // Fill the entire screen width
+    //       objectFit: "cover",
+    //       position: "absolute",
+    //       top: "0",
+    //       left: "0",
+    //       zIndex: -1,
+    //     }}
+    //   ></video>
+
+    //   <Box
+    //     position="absolute"
+    //     top="50%"
+    //     left="50%"
+    //     transform="translate(-50%, -50%)"
+    //   >
+    //     <Image
+    //       src={"/7Logo.png"}
+    //       alt={"Logo"}
+    //       boxSize={boxSize}
+    //       zIndex={99}
+    //       marginBottom="20px"
+    //       opacity={0.9}
+    //     />
+    //     <VStack spacing={3} textAlign="center" width="full">
+    //       <Heading
+    //         as="h2"
+    //         size={headingSize}
+    //         color="#CDAF00"
+    //         zIndex={99}
+    //         backgroundColor={"#4900CD"}
+    //         flexWrap={"nowrap"}
+    //         dropShadow={"100px"}
+    //         padding={"5px"}
+    //       >
+    //         Sheffield
+    //       </Heading>
+    //       <Text
+    //         fontSize={headingSize}
+    //         padding={"5px"}
+    //         color="white"
+    //         backgroundColor={"#4900CD"}
+    //       >
+    //         31/08/2024 - 01/09/2024
+    //       </Text>
+    //       <Heading
+    //         as="h2"
+    //         size={headingSize}
+    //         color="#CDAF00"
+    //         zIndex={99}
+    //         flexWrap={"nowrap"}
+    //         padding={"5px"}
+    //         backgroundColor={"#4900CD"}
+    //       >
+    //         Kelham Island Museum
+    //       </Heading>
+    //     </VStack>
+    //   </Box>
+    // </Box>
   );
 };
