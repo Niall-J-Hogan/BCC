c0c3806be0f458a0fd87c6d47bf4f13200f10bd3
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-09-10 19:26:11 +0100

    fix: added navbar component to page

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -15,44 +15,16 @@ import React from "react";
 import { HamburgerMenu } from "@/components/HamburgerMenu";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { SlSocialInstagram } from "react-icons/sl";
+import { NavBarLogo } from "../NavBarLogo";
 
 export const NavBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef(null);
 
   return (
-    <Box
-      as="nav"
-      bg="#5B19D2"
-      // opacity={0.9}
-      p={4}
-      color="white"
-      position="fixed"
-      w="100%"
-    >
+    <Box as="nav" bg="#5B19D2" p={4} color="white" position="fixed" w="100%">
       <Flex maxW="1200px" mx="auto" alignItems="center" position="relative">
-        <Box
-          position="absolute"
-          top="-10px"
-          overflow="hidden"
-          width="150px"
-          height="150px"
-          display={{ base: "none", md: "flex" }}
-        >
-          <Image src="/7Logo.png" alt="Logo" boxSize="100%" />
-        </Box>
-        <Box
-          position="absolute"
-          left="10px"
-          top="-10px"
-          overflow="hidden"
-          width="100px"
-          height="100px"
-          display={{ base: "flex", md: "none" }}
-        >
-          <Image src="/7Logo.png" alt="Logo" boxSize="100%" />
-        </Box>
-
+        <NavBarLogo />
         <Spacer />
         <HStack spacing={6} display={{ base: "none", md: "flex" }}>
           <Link fontWeight="semibold">Home</Link>
@@ -63,16 +35,15 @@ export const NavBar = () => {
             <SlSocialInstagram size={32} color="white" />
           </Link>
         </HStack>
-        <Center>
-          <IconButton
-            ref={btnRef}
-            aria-label="Open Menu"
-            icon={<GiHamburgerMenu />}
-            onClick={onOpen}
-            bg="blue.500"
-            display={{ base: "flex", md: "none" }}
-          />
-        </Center>
+        <IconButton
+          ref={btnRef}
+          aria-label="Open Menu"
+          color={"white"}
+          icon={<GiHamburgerMenu />}
+          onClick={onOpen}
+          bg="#979BFF"
+          display={{ base: "flex", md: "none" }}
+        />
       </Flex>
       <HamburgerMenu isOpen={isOpen} onClose={onClose} btnRef={btnRef} />
     </Box>
