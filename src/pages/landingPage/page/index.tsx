81b67e82936d097cf567e6a1e5f363aa7ad1db75
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-09-10 16:24:14 +0100

    feat: page

--- src/pages/landingPage/page/index.tsx
+++ src/pages/landingPage/page/index.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import { VStack } from "@chakra-ui/react";
+
+import { NavBar } from "@/components/NavBar";
 
 export const LandingPage = () => {
-  return <div>index</div>;
+  return (
+    <VStack w="full" h="full">
+      <NavBar />
+    </VStack>
+  );
 };
