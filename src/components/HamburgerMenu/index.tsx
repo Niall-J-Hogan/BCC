0536b340e4796a2ea9444fa7df92d74b4fc6fdca
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-09-10 19:25:55 +0100

    fix: hamburger formatting

--- src/components/HamburgerMenu/index.tsx
+++ src/components/HamburgerMenu/index.tsx
@@ -3,8 +3,8 @@ import {
   DrawerBody,
   DrawerCloseButton,
   DrawerContent,
-  DrawerHeader,
   DrawerOverlay,
+  HStack,
   Link,
   VStack,
 } from "@chakra-ui/react";
@@ -18,20 +18,27 @@ export const HamburgerMenu = ({ isOpen, onClose, btnRef }: IHamburgerMenu) => {
   return (
     <Drawer
       isOpen={isOpen}
-      placement="right"
+      placement="top"
       onClose={onClose}
       finalFocusRef={btnRef}
     >
       <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerHeader>Menu</DrawerHeader>
-        <DrawerBody>
-          <VStack spacing={6} width="auto">
-            <Link>Home</Link>
-            <Link>Artists</Link>
-            <Link>Vendors</Link>
-            <Link>Contact</Link>
+      <DrawerContent
+        backgroundColor={"#979BFF"}
+        borderRadius={"10px"}
+        width="120px"
+        marginLeft={"auto"}
+        py="30px"
+      >
+        <HStack height="50px">
+          <DrawerCloseButton />
+        </HStack>
+        <DrawerBody margin={"auto"}>
+          <VStack spacing={6} width="auto" p="0px" margin={"auto"}>
+            <Link color={"white"}>Home</Link>
+            <Link color={"white"}>Artists</Link>
+            <Link color={"white"}>Vendors</Link>
+            <Link color={"white"}>Contact</Link>
           </VStack>
         </DrawerBody>
       </DrawerContent>
