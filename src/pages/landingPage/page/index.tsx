540225b83f7f5936bf734f062427b8dc40cb3ac7
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-11-04 11:58:40 +0000

    fix: adjustments

--- src/pages/landingPage/page/index.tsx
+++ src/pages/landingPage/page/index.tsx
@@ -6,32 +6,23 @@ import { NavBar } from "@/components/NavBar";
 import { SponsorLogoContainer } from "@/components/SponsorLogoContainer";
 
 export const LandingPage = () => {
-  const headerMargin = useBreakpointValue({
-    base: "70px", // Mobile devices
-    md: "65px", // Large screens and up
-  });
-
   return (
-    <>
-      <VStack w="full" h="full" spacing={5}>
+    <VStack spacing={0} backgroundColor={"#E4FDE1"}>
+      <VStack w="full" h="full">
         <NavBar />
       </VStack>
-      <VStack marginTop={headerMargin} height="full">
-        <HeaderGif
-          title={"A Tattoo Convention by Tattooers, for Sheffield!"}
-          subtitle={"31/08/2024 - 01/09/2024"}
-          tagline="Kelham Island Museum"
-        />
-      </VStack>
-      <VStack
-        height="1200px"
-        backgroundColor={"#E4FDE1"}
-        justifyContent={"flex-start"}
-      >
-        <SponsorLogoContainer />
-      </VStack>
+
+      <HeaderGif
+        title={"A Tattoo Convention by Tattooers, for Sheffield!"}
+        subtitle={"31/08/2024 - 01/09/2024"}
+        tagline="Kelham Island Museum"
+      />
+
+      <SponsorLogoContainer />
+
+      <VStack height="1200px" width="80%" border={"solid 2px black"}></VStack>
 
       <Footer />
-    </>
+    </VStack>
   );
 };
