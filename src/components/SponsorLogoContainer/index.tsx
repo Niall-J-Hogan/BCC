ac497745f962d2847dd70eb2731943213bc8672a
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-11-04 11:32:04 +0000

    feat: links and styling to sponsor container

--- src/components/SponsorLogoContainer/index.tsx
+++ src/components/SponsorLogoContainer/index.tsx
@@ -1,16 +1,57 @@
-import { HStack, Image, VStack } from "@chakra-ui/react";
+import { HStack, Text, Image, VStack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 export const SponsorLogoContainer = () => {
+  const router = useRouter();
   return (
     <VStack>
       <HStack
-        width={"50vw"}
+        width={"60vw"}
         height="200px"
         display={"flex"}
         justifyContent={"space-between"}
       >
-        <Image src="/barberLogo.png" alt="" height="inherit" py="5px" />
-        <Image src="/ogLogo.png" alt="" height="inherit" py="5px" />
-        <Image src="/LDlogo.png" alt="" height="inherit" py="5px" />
+        <VStack width={"50vw"} height="200px">
+          <Image
+            src="/barberLogo.png"
+            alt=""
+            height="inherit"
+            py="5px"
+            onClick={() => router.push("https://www.barberdts.com/uk/")}
+            cursor={"pointer"}
+          />
+          <Text fontSize={"xl"}>Barbers</Text>
+          <Text fontSize={"md"} fontStyle={"italic"}>
+            Worldwide Tattoo Supplies
+          </Text>
+        </VStack>
+        <VStack width={"50vw"} height="200px">
+          <Image
+            src="/ogLogo.png"
+            alt=""
+            height="inherit"
+            py="5px"
+            onClick={() => router.push("https://www.ogtattoosupply.com/")}
+            cursor={"pointer"}
+          />
+          <Text fontSize={"xl"}>OG Supply</Text>
+          <Text fontSize={"md"} fontStyle={"italic"}>
+            Tattoo Machines and Supplies
+          </Text>
+        </VStack>
+        <VStack width={"50vw"} height="200px">
+          <Image
+            src="/LDlogo.png"
+            alt=""
+            height="inherit"
+            py="5px"
+            onClick={() => router.push("https://liquiddeath.com/en-gb")}
+            cursor={"pointer"}
+          />
+          <Text fontSize={"xl"}>Liquid Death</Text>
+          <Text fontSize={"md"} fontStyle={"italic"}>
+            Environmentally Friendly Water
+          </Text>
+        </VStack>
       </HStack>
     </VStack>
   );
