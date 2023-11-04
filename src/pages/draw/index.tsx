f0bcfacd9c494da53a50da4569e26e75d322e2c0
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-11-04 11:58:58 +0000

    test drawing app

---
+++ src/pages/draw/index.tsx
@@ -0,0 +1,27 @@
+import { VStack, useBreakpointValue } from "@chakra-ui/react";
+
+import { Footer } from "@/components/Footer";
+import { HeaderGif } from "@/components/HeaderGif";
+import { NavBar } from "@/components/NavBar";
+import { SponsorLogoContainer } from "@/components/SponsorLogoContainer";
+import { DrawingApp } from "@/components/PaintComponent/App";
+
+export const Draw = () => {
+  const headerMargin = useBreakpointValue({
+    base: "70px", // Mobile devices
+    md: "65px", // Large screens and up
+  });
+
+  return (
+    <>
+      <VStack w="full" h="full" spacing={5}>
+        <NavBar />
+      </VStack>
+      <VStack marginTop={headerMargin} height="full">
+        <DrawingApp />
+      </VStack>
+
+      <Footer />
+    </>
+  );
+};
