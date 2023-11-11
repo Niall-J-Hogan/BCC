05a3d1e8f0213efcff1cacf9d168d7655c8dee0c
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-11-11 14:55:12 +0000

    fix: added text to sponsor bar

--- src/components/SponsorLogoContainer/index.tsx
+++ src/components/SponsorLogoContainer/index.tsx
@@ -1,4 +1,4 @@
-import { HStack, useBreakpointValue } from "@chakra-ui/react";
+import { HStack, Heading, VStack, useBreakpointValue } from "@chakra-ui/react";
 import { SponsorLogoBox } from "../SponsorLogoBox/index";
 export const SponsorLogoContainer = () => {
   const widthSize = useBreakpointValue({
@@ -7,33 +7,45 @@ export const SponsorLogoContainer = () => {
   });
 
   return (
-    <HStack
-      width={widthSize}
-      height="full"
-      display={"flex"}
-      justifyContent={"space-between"}
-    >
-      <SponsorLogoBox
-        title="Barbers"
-        subtitle="Worldwide Tattoo Supplies"
-        altText="Barber Tattoo Supplies"
-        imageLink="/barberLogo.png"
-        websiteLink="https://www.barberdts.com/uk/"
-      />
-      <SponsorLogoBox
-        title="OG Supply"
-        subtitle="   Tattoo Machines and Supplies"
-        altText="OG Tattoo Supply"
-        imageLink="/ogLogo.png"
-        websiteLink="https://www.ogtattoosupply.com/"
-      />
-      <SponsorLogoBox
-        title="Liquid Death"
-        subtitle="Environmentally Friendly Water"
-        altText="Liquid Death Mountain Water"
-        imageLink="/LDlogo.png"
-        websiteLink="https://liquiddeath.com/en-gb"
-      />
-    </HStack>
+    <VStack width={"full"} marginBottom={"50px"}>
+      <Heading
+        as="h2"
+        size={"xl"}
+        color="White "
+        fontStyle={"italic"}
+        backgroundColor={"#4900CD"}
+        padding="5px"
+      >
+        Proudly Supported By
+      </Heading>
+      <HStack
+        width={widthSize}
+        height="full"
+        display={"flex"}
+        justifyContent={"space-between"}
+      >
+        <SponsorLogoBox
+          title="Barbers"
+          subtitle="Worldwide Tattoo Supplies"
+          altText="Barber Tattoo Supplies"
+          imageLink="/barberLogo.png"
+          websiteLink="https://www.barberdts.com/uk/"
+        />
+        <SponsorLogoBox
+          title="OG Supply"
+          subtitle="   Tattoo Machines and Supplies"
+          altText="OG Tattoo Supply"
+          imageLink="/ogLogo.png"
+          websiteLink="https://www.ogtattoosupply.com/"
+        />
+        <SponsorLogoBox
+          title="Liquid Death"
+          subtitle="Environmentally Friendly Water"
+          altText="Liquid Death Mountain Water"
+          imageLink="/LDlogo.png"
+          websiteLink="https://liquiddeath.com/en-gb"
+        />
+      </HStack>
+    </VStack>
   );
 };
