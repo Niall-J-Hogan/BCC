2942354d23f02d2faf4c3250ed3a7c0097eeedd4
Niall_Hogan <b9013287@my.shu.ac.uk>
2023-11-14 11:15:29 +0000

    fix: favicon and title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/sevenLogo.png" sizes="any" />
+      <title>7Hills Tattoo Convention</title>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
