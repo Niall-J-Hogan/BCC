e07590fbb17c0ae5e72b6fedd7e95ab9be8fc60d
Niall_Hogan <b9013287@my.shu.ac.uk>
2024-02-03 18:07:02 +0000

    feat: artist page

--- src/pages/artists/page/index.tsx
+++ src/pages/artists/page/index.tsx
@@ -1,11 +1,17 @@
-import { Button, VStack } from "@chakra-ui/react";
+import { Heading, VStack, Text, HStack } from "@chakra-ui/react";
 
 import { Footer } from "@/components/Footer";
 import { HeaderGif } from "@/components/HeaderGif";
 import { NavBar } from "@/components/NavBar";
 import { SponsorLogoContainer } from "@/components/SponsorLogoContainer";
+import { UseGetIsDevice } from "@/hooks/useGetIsDevice";
+import { UseGetSizeForDevices } from "@/hooks/useGetSizesForDevice";
+import { copy } from "../data";
 
 export const ArtistPage = () => {
+  const { isIpad, isIpadHorizontal, isMobile } = UseGetIsDevice();
+  const { sponsorLogoContainer } = UseGetSizeForDevices();
+
   return (
     <VStack spacing={0} backgroundColor={"#E4FDE1"}>
       <VStack w="full" h="full">
@@ -13,23 +19,76 @@ export const ArtistPage = () => {
       </VStack>
 
       <HeaderGif
-        title="Artists"
-        subtitle="We have a range of artists from around the world, covering every style."
+        title={"Seven Hills Tattoo Convention"}
+        subtitle={"31/08/2024 - 01/09/2024"}
+        tagline="Kelham Island Museum"
         isButton={true}
-        tagline={
-          <Button
-            size={"lg"}
-            width="200px"
-            color={"white"}
-            backgroundColor={"#CDAF00"}
-          >
-            Apply as an Artist
-          </Button>
-        }
       />
-      <SponsorLogoContainer />
 
-      <VStack height="1200px" width="80%" border={"solid 2px black"}></VStack>
+      <VStack height="auto" width="80%" paddingY={"50px"} gap={10}>
+        <Heading
+          as="h1"
+          size={"2xl"}
+          color="black  "
+          padding={"5px"}
+          fontStyle={"italic"}
+          alignSelf={"center"}
+          borderBottom={"solid 2px black"}
+        >
+          Artists
+        </Heading>
+        <HStack width={isMobile || isIpad || isIpadHorizontal ? "100%" : "70%"}>
+          {isMobile || isIpad || isIpadHorizontal ? (
+            <Text
+              as="article"
+              lineHeight={"9"}
+              fontWeight={"semibold"}
+              whiteSpace="pre-line"
+              py="10px"
+              textAlign={"center"}
+            >
+              {copy}
+            </Text>
+          ) : (
+            <Text
+              as="article"
+              paddingX="40px"
+              lineHeight={"9"}
+              fontWeight={"semibold"}
+              whiteSpace="pre-line"
+              align={"center"}
+            >
+              {copy}
+            </Text>
+          )}
+        </HStack>
+
+        {isMobile || isIpad || isIpadHorizontal ? (
+          <Heading
+            as="h2"
+            size={sponsorLogoContainer.headingSize}
+            color="White"
+            fontStyle={"italic"}
+            backgroundColor={"red"}
+            padding="10px"
+          >
+            ARTIST LIST COMING SOON!
+          </Heading>
+        ) : (
+          <Heading
+            as="h2"
+            size={sponsorLogoContainer.headingSize}
+            color="White"
+            fontStyle={"italic"}
+            backgroundColor={"red"}
+            padding="20px"
+            px="300px"
+          >
+            FULL ARTIST LIST COMING SOON!
+          </Heading>
+        )}
+      </VStack>
+      <SponsorLogoContainer />
 
       <Footer />
     </VStack>
