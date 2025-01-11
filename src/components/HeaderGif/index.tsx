2191f2a2eedd0f0c63b81dee57394532f58fdbd9
Niall_Hogan <b9013287@my.shu.ac.uk>
2025-01-11 14:53:04 +0000

    added AA to header gif

--- src/components/HeaderGif/index.tsx
+++ src/components/HeaderGif/index.tsx
@@ -78,13 +78,10 @@ export const HeaderGif = ({
               backgroundColor={"#CDAF00"}
               size={"lg"}
               width={headerGif.gifButtonSize}
-              onClick={() =>
-                handleClick(
-                  "https://www.skiddle.com/whats-on/Sheffield/Kelham-Island-Museum/Seven-Hills-Tattoo-Convention-2024-Sheffield/38180081/"
-                )
-              }
+              onClick={() => handleClick("https://forms.gle/fi4GdsigSCcKn23S9")}
+              _hover={{ bg: "#5B19D2" }}
             >
-              Buy Tickets
+              Artist Application
             </Button>
           )}
         </VStack>
